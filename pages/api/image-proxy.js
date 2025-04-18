460b7cc2bbf500d54972990bdd50a01ae2df4954
Daniel Savage <danielsavage@Mac.lan>
2025-04-18 15:29:03 -0700

    Fix broken API endpoints by adding routes for all critical API paths

--- pages/api/image-proxy.js
+++ pages/api/image-proxy.js
@@ -3,6 +3,16 @@
  * Optimized for Vercel deployment
  */
 
+// API route for image proxying
+// Forwards to the all-in-one.js handler
+
+const allInOne = require('../../api/all-in-one.js');
+
+export default async function handler(req, res) {
+  console.log('[Vercel] Image proxy API route called');
+  return allInOne(req, res);
+}
+
 export const config = {
   api: {
     bodyParser: false,
